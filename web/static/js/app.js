c36a7834728d5f624f600e165818d6aa14f75298
Seth Etter <sethetter@gmail.com>
2016-09-13 10:26:18 -0500

    Base setup for Elm, currently not compiling

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,3 +19,7 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+import Elm from "./main";
+const elmDiv = document.getElementById("elm-target");
+if (elmDiv) Elm.Main.embed(elmDiv);
+
