369820f0e04c5718aa3c40541f83981b17c28da9
Tanner Doshier <git@doshitan.com>
2016-10-07 11:01:07 -0500

    Add header and footer to app
    Summary:
    Adds a header which says some stuff and credits the OpenGov Foundation at the
    bottom of the app.
    
    Test Plan: Build it. Look at it.
    
    Reviewers: sethetter
    
    Reviewed By: sethetter
    
    Subscribers: scout
    
    Maniphest Tasks: T54
    
    Differential Revision: https://phabricator.opengovfoundation.org/D22

--- client/src/App/View.elm
+++ client/src/App/View.elm
@@ -4,6 +4,7 @@ import App.Model exposing (..)
 import App.Update exposing (..)
 import Html.App as Html
 import Html exposing (..)
+import Html.Attributes exposing (..)
 import States.EditDoc
 import States.Login
 import States.PageNotFound
@@ -11,6 +12,34 @@ import States.PageNotFound
 
 view : Model -> Html Msg
 view model =
+    div []
+        [ viewHeader model
+        , viewState model
+        , viewFooter model
+        ]
+
+
+viewHeader : Model -> Html Msg
+viewHeader model =
+    case model.activeState of
+        Login _ ->
+            div [] []
+
+        _ ->
+            div [ class "app-header", attribute "role" "banner" ]
+                [ div [ class "logo" ]
+                    [ em [ class "logo-text" ]
+                        [ a [ href "/" ]
+                            [ span [ class "app-name" ] [ text "AssemblyWorks" ]
+                            , text "City of Chicago Commemorative Resolutions"
+                            ]
+                        ]
+                    ]
+                ]
+
+
+viewState : Model -> Html Msg
+viewState model =
     case model.activeState of
         Login state ->
             Html.map loginTranslator <| States.Login.view state
@@ -20,3 +49,16 @@ view model =
 
         PageNotFound ->
             Html.map (always NoOp) <| States.PageNotFound.view
+
+
+viewFooter : Model -> Html Msg
+viewFooter model =
+    footer [ class "app-footer", attribute "role" "contentinfo" ]
+        [ div [ class "pull-right" ]
+            [ text "Built by "
+            , a
+                [ href "https://opengovfoundation.org"
+                ]
+                [ text "The OpenGov Foundation" ]
+            ]
+        ]
