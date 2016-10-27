800a6e674fdbe9b87c5408e901d1b10d74a2f0a1
Tanner Doshier <git@doshitan.com>
2016-10-27 15:53:32 -0500

    sslSupport -> enableSSL

--- config/resolutionizer.nix
+++ config/resolutionizer.nix
@@ -111,7 +111,7 @@ in {
       '';
     };
 
-    sslSupport = mkOption {
+    enableSSL = mkOption {
       type = types.bool;
       default = true;
       description = ''
@@ -150,15 +150,15 @@ in {
 
       recommendedOptimisation = true;
       recommendedGzipSettings = true;
-      recommendedTlsSettings = cfg.sslSupport;
+      recommendedTlsSettings = cfg.enableSSL;
       sslProtocols = "TLSv1.2 TLSv1.1 TLSv1";
       sslCiphers = "ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:ECDHE-RSA-RC4-SHA:ECDHE-ECDSA-RC4-SHA:AES128:AES256:RC4-SHA:HIGH:!aNULL:!eNULL:!EXPORT:!DES:!3DES:!MD5:!PSK";
 
       virtualHosts = {
         ${cfg.domainName} = {
             default = true;
-            forceSSL = cfg.sslSupport;
-            enableACME = cfg.sslSupport;
+            forceSSL = cfg.enableSSL;
+            enableACME = cfg.enableSSL;
             acmeRoot = "/var/www/challenges"; # TODO: this might need to be /var/www/challenges/acme-challenge
 
             locations."/" = {
