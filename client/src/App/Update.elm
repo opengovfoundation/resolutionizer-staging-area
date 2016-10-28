3ae94edb55f186e3c5dc0a360de46c9ee84d22a8
Tanner Doshier <git@doshitan.com>
2016-10-28 13:30:54 -0500

    Add basic route checking/reject invalid route changes

--- client/src/App/Update.elm
+++ client/src/App/Update.elm
@@ -75,23 +75,18 @@ update msg model =
 
                     EditDocR route' ->
                         let
-                            -- TODO: move this stuff into the EditDoc module? Some
-                            -- routes we may be able to just go to, others we may
-                            -- want to forbid users from just jumping to them (e.g.
-                            -- filling out clauses before the details), that logic
-                            -- has to live somewhere when we implement it
-                            ( editDocState, editDocCmd ) =
+                            currentEditDocState =
                                 case model.activeState of
                                     EditDoc state ->
-                                        ( state, Cmd.none )
+                                        Just state
 
                                     _ ->
-                                        (States.EditDoc.init Doc.emptyDoc)
+                                        Nothing
 
-                            newActiveState =
-                                EditDoc { editDocState | activeRoute = route' }
+                            ( editDocState, editDocCmd ) =
+                                States.EditDoc.doRoute route' currentEditDocState
                         in
-                            ( { model | activeState = newActiveState }, Cmd.map EditDocMsg editDocCmd )
+                            ( { model | activeState = EditDoc editDocState }, Cmd.map EditDocMsg editDocCmd )
 
         LoggedIn ->
             let
