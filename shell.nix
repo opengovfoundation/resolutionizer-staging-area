519d0cd58aee8b84f3f253e8558c1b180be49273
Tanner Doshier <git@doshitan.com>
2016-10-27 19:38:20 -0500

    Support non-AWS deployment targets for nix stuff
    Summary:
    The existing resolutionizer description only really works for production
    deployments to AWS. It's useful to support local VM or container setups too for
    testing deployments and such. Changes needed are:
    
    - Support automatic setup of a local postgres database
    - Support non-ssl setups (and refactor nginx config to use more nixos stuff to make it easier)
    - Add option to specify AWS access credentials explicity
    
    Also adding a libvirt nixops example that I've been using and helper scripts for
    managing different nixops state files. Could probably have the `.envrc` file
    default to the `local.nixops` file so locally one can just use `nixops`,
    reserving `bin/nixops-prod` for managing production/external nixops networks.
    
    Also threw in a top level `shell.nix` file to easily drop into a shell with all
    the dev tools (for both the client and server) with just a `nix-shell` from the
    root directory.
    
    Test Plan:
    Mostly just look at it, but if you are feeling adventurous:
    - Install libvirt (`nix-env -i libvirt` might work, using distro packages is probably safer)
    - Make a new nixops network with the libvirt config (`bin/nixops-local create '<config/system-libvirt.nix>' -d libvirt`)
    - Set all the deployment parameters (`bin/nixops-local set-args --argstr "domainName" "test.example.org" --argstr "awsRegion" "<region for s3 bucket>" --argstr "s3BucketName" "<development bucket name>" --argstr "awsAccessKeyId" "<some access key>" --argstr "awsSecretAccessKey" "<some secret>" -d libvirt`)
    - Deploy the new network (`bin/nixops-local deploy -d libvirt`)
    - Open up the VM's IP in your browser (`bin/nixops-local info -d libvirt` will show it)
    - Ensure the app works, assuming you've provided valid AWS credentials
    
    Reviewers: sethetter
    
    Differential Revision: https://phabricator.opengovfoundation.org/D34

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? (import <nixpkgs> {})
+, noDevTools ? false
+}:
+
+with pkgs;
+let
+  builds = callPackage ./. { };
+  devTools = [ elmPackages.elm-format elmPackages.elm-reactor elmPackages.elm-repl ];
+  unifiedEnv = stdenv.mkDerivation {
+    name = "resolutionizer";
+    buildInputs = if noDevTools then [] else devTools;
+    # The `buildInputs` attrs of the derivations are empty at this point, but
+    # the derivations needed are stored in the `nativeBuildInputs` attr, this is
+    # a hack
+    nativeBuildInputs = builds.server.nativeBuildInputs ++ builds.client.nativeBuildInputs;
+  };
+in
+  unifiedEnv
