d572b74e797f774516dc74e1d112c5030dda396c
Tanner Doshier <git@doshitan.com>
2016-11-14 16:00:39 -0600

    Add bulk clause importer prototype with UI
    Summary:
    Don't know quite how to present the bulk import option in the UI. Don't really
    want to throw another button onto the screen. The bulk import page could also
    use better explanatory text.
    
    There is some miscellaneous refactoring in the client code, moving to standalone
    RemoteData package, API Request types instead of having lots of `RemoteData
    <stuff>` everywhere and such.
    
    The bulk import tool is currently a separate tool written in Haskell. Could be
    migrated into some Elixir code eventually, but was not impressed by the
    available tooling and this is fairly self contained, so shouldn't be an issue
    for now.
    
    Test Plan:
    - Build and install the new client (`nix-shell --command "cd client && make build && make install"`)
    - Run the server (`nix-shell --command "cd server && mix phoenix.server"`)
    - Get to the bulk clause input page
    - For reasonably well formatted clause text, it should succeed and put you back at the normal clause screen
    
    Reviewers: sethetter
    
    Maniphest Tasks: T79
    
    Differential Revision: https://phabricator.opengovfoundation.org/D45

--- bulk-clause-import/Main.hs
+++ bulk-clause-import/Main.hs
@@ -3,10 +3,9 @@ module Main where
 import qualified BulkClauseImport
 import qualified Data.Aeson           as Aeson
 import qualified Data.ByteString.Lazy as ByteString
-import           Data.Either          (Either (..))
 import qualified Data.Either
 import qualified Data.List
-import           Prelude              (IO, (.), ($))
+import           Prelude              (IO, ($))
 import qualified System.Environment
 import qualified System.Exit
 import qualified System.IO
@@ -17,14 +16,6 @@ main = do
   input <- case args of
     [] -> System.IO.getContents
     file : _ -> System.IO.readFile file
-
-  -- case BulkClauseImport.parseClauses input of
-  --   Right clauseData -> do
-  --     ByteString.putStr (Aeson.encode clauseData)
-  --     System.Exit.exitSuccess
-  --   Left errMsg -> do
-  --     System.IO.putStr errMsg
-  --     System.Exit.exitFailure
 
   let parseResult = BulkClauseImport.parseClausesPerClause input
   case Data.Either.partitionEithers parseResult of
--- server/mix.exs
+++ server/mix.exs
@@ -21,7 +21,8 @@ defmodule Resolutionizer.Mixfile do
     [mod: {Resolutionizer, []},
      applications: [:phoenix, :phoenix_pubsub, :cowboy, :logger, :gettext,
                     :phoenix_ecto, :postgrex, :timex, :ex_machina, :ex_aws,
-                    :poison, :arc, :arc_ecto, :hackney, :sweet_xml, :gen_stage]]
+                    :poison, :arc, :arc_ecto, :hackney, :sweet_xml, :gen_stage,
+                    :temp]]
   end
 
   # Specifies which paths to compile per environment.
